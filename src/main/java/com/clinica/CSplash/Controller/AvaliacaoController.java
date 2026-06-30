a25d44369eaa68848610c1fbf72eb3a55b1b6813
Francisco <franciscomauricio796@gmail.com>
2026-06-30 14:08:45 -0300

    feat:construcao do Service(Avaliacao)

---
+++ src/main/java/com/clinica/CSplash/Controller/AvaliacaoController.java
@@ -0,0 +1,13 @@
+package com.clinica.CSplash.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("*")
+@RequestMapping("/api/avalicao")
+
+public class AvaliacaoController {
+
+
+}
---
+++ src/main/java/com/clinica/CSplash/Service/AvaliacaoService.java
@@ -0,0 +1,57 @@
+package com.clinica.CSplash.Service;
+
+
+import com.clinica.CSplash.DTO.Request.AvalicaoRequest;
+import com.clinica.CSplash.DTO.Response.AvaliacaoResponse;
+import com.clinica.CSplash.Model.Avaliacao;
+import com.clinica.CSplash.Repository.AvaliacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class AvaliacaoService {
+
+    @Autowired
+    private AvaliacaoRepository avaliacaoRepository;
+
+    public AvaliacaoResponse criarAvalicao(AvalicaoRequest avalicaoRequest){
+        Avaliacao avaliacao=new Avaliacao();
+
+        avaliacao.setLocacao(avalicaoRequest.locacao());
+        avaliacao.setUsuario(avalicaoRequest.usuario());
+        avaliacao.setNota(avalicaoRequest.nota());
+        avaliacao.setComentario(avalicaoRequest.comentario());
+
+        return toResponse (avaliacaoRepository.save(avaliacao));
+    }
+
+    public List<AvaliacaoResponse> listarAvaliacao(){
+        return avaliacaoRepository.findAll().stream().map(this::toResponse).toList();
+    }
+    public AvaliacaoResponse listarAvaliacaoPorId(UUID id){
+        Avaliacao avaliacao=avaliacaoRepository.findById(id).orElseThrow(()->new RuntimeException("ID não encontrado"));
+
+        return toResponse(avaliacao);
+    }
+
+    public void  apagarAvaliacao(UUID id){
+        Avaliacao avaliacao=avaliacaoRepository.findById(id).orElseThrow(()->new RuntimeException("ID não encontrado"));
+
+       avaliacaoRepository.delete(avaliacao);
+    }
+    public AvaliacaoResponse toResponse(Avaliacao avaliacao){
+        return new AvaliacaoResponse(
+                avaliacao.getId(),
+                avaliacao.getLocacao(),
+                avaliacao.getUsuario(),
+                avaliacao.getNota(),
+                avaliacao.getComentario()
+
+
+
+        );
+    }
+}
